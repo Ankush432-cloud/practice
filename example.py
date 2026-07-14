4d18244f6eb0a55bf92a6540eeade6906f2333ec
Ashraka-Gampawar <ashrakagampawar@gmail.com>
2026-07-14 16:54:31 +0530

    Added sub function

--- example.py
+++ example.py
@@ -4,8 +4,16 @@ class operation :
         self.c = self.a + self.b
         return self.c
     
+    def sub(self,p,q):
+        self.r=self.p-self.q
+        return self.r
+
 obj = operation()
 
 result = obj.add(a=10 , b= 100)
+result2 = obj.sub(p=20, q=10)
 
 print(result)
+print(result2)
+
+
