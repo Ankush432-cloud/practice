4418ca807396aed0c4a2bf893f752fb10c085e5e
Gayu 20-04 <gayatrichavhan687@gmail.com>
2026-07-14 20:20:40 +0530

    Added Multiple Function

--- example.py
+++ example.py
@@ -1,19 +1,25 @@
 # function for add two number.
 class operation :
     def add(self,a,b):
-        self.c = self.a + self.b
+        self.c = a + b
         return self.c
     
     def sub(self,p,q):
-        self.r=self.p-self.q
+        self.r= p-q
         return self.r
+    
+    def mul(self,f,d):
+        self.e = f * d 
+        return self.e
 
 obj = operation()
 
 result = obj.add(a=10 , b= 100)
 result2 = obj.sub(p=20, q=10)
+result3 = obj.mul(f=12 , d=10)
 
 print(result)
 print(result2)
+print(result3)
 
 
